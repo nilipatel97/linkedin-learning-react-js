3a0df85f0039c5f4c25487feabc0ec9c987bbdf6
Nili Patel <88466745+nilipatel97@users.noreply.github.com>
2022-05-09 20:49:11 -0400

    added two examples of rendering content using  react.createElement

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,8 +5,14 @@ import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  React.createElement("h1", null, "hellooooo!!!")
+  React.createElement("h1", null, "good morning!!!")
 );
+
+const root2 = ReactDOM.createRoot(document.getElementById("root2"));
+
+root2.render(
+  React.createElement("div", {style: {backgroundColor:"pink"}}, React.createElement("h2", {style:{color: "green"}}, "good afternoon!!!!~"))
+)
 
 
 
@@ -20,3 +26,5 @@ root.render(
   // 3) store that root in a const
 
   // root.render(React.createElement(a,b,c))
+
+  //CONCLUSION -- react.createElement calls get very unweildy ... jsx helps with this
